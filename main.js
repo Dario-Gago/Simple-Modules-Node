ef9d6505cc1c02c01db8d0f675f5b83056ccb524
Dario-Gago <gagodario1@gmail.com>
2024-09-18 15:39:13 -0300

    Agregando los modulos de node

---
+++ main.js
@@ -0,0 +1,4 @@
+const web = require('./modules/myModules');
+console.log(web);
+const {myArray} = require('./modules/myModules');
+console.log(myArray);
---
+++ modules/myModules.js
@@ -0,0 +1,16 @@
+const myWebAddress = 'dariogago.cl';
+const age = 19;
+const myArray = [10,20,30];
+const users = {
+    name: 'Ryan',
+    lastname: 'Ray'
+}
+
+const group = {
+    myWebAddress,
+    age,
+    myArray,
+    users
+}
+//coloco lo que quiero exportar
+module.exports = group;
